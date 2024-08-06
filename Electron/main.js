a7dd4bedefd5da48340994d58a96fd2670bfb63b
Sudarsan <s_r@mt.iitr.ac.in>
2024-08-06 20:24:14 +0530

    previous version

--- Electron/main.js
+++ Electron/main.js
@@ -1,6 +1,7 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 require("./ipcHandlers/sockets.js");
-const { app, BrowserWindow } = require("electron");
+
+const { app, BrowserWindow, Tray, Menu } = require("electron");
 const path = require("path");
 const Store = require("electron-store");
 const store = new Store();
@@ -11,13 +12,14 @@ const { loginIPC } = require("./ipcHandlers/loginHandlers.js");
 const { monitorIPC } = require("./ipcHandlers/monitoringHandlers.js");
 
 let mainWindow;
+let tray = null;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 600,
-    height: 400,
+    width: 650,
+    height: 500,
     autoHideMenuBar: true,
-    // frame: false,
+    maximizable: false,
     icon: path.join(__dirname, "./Icons/ss.ico"),
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
@@ -32,9 +34,48 @@ function createWindow() {
   //   : `file://${path.join(__dirname, "../Frontend/build/index.html")}`; // Prod mode URL
 
   mainWindow.loadURL(`file://${path.join(__dirname, "./build/index.html")}`);
+  // mainWindow.loadURL("http://localhost:3001");
 
   mainWindow.on("closed", () => {
     mainWindow = null;
+  });
+
+  mainWindow.on("minimize", (event) => {
+    event.preventDefault();
+    mainWindow.hide();
+  });
+
+  mainWindow.on("close", (event) => {
+    if (!app.isQuiting) {
+      event.preventDefault();
+      mainWindow.hide();
+    }
+    return false;
+  });
+}
+
+function createTray() {
+  tray = new Tray(path.join(__dirname, "./Icons/ss.ico"));
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Show Application",
+      click: () => {
+        mainWindow.show();
+      },
+    },
+    {
+      label: "Exit",
+      click: () => {
+        app.isQuiting = true;
+        app.quit();
+      },
+    },
+  ]);
+  tray.setToolTip("Endpoint Metrics Agent");
+  tray.setContextMenu(contextMenu);
+
+  tray.on("click", () => {
+    mainWindow.show();
   });
 }
 
@@ -42,6 +83,7 @@ app.disableHardwareAcceleration();
 
 app.whenReady().then(() => {
   createWindow();
+  createTray();
 
   app.on("activate", () => {
     // On macOS it's common to re-create a window in the app when the
@@ -56,8 +98,6 @@ app.whenReady().then(() => {
 });
 
 app.on("window-all-closed", () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
     app.quit();
   }
