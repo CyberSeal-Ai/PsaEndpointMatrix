5f8b850a2a6218b84cf38dd1223881cec2dff57e
Sudarsan <s_r@mt.iitr.ac.in>
2024-08-22 13:00:07 +0530

    mac#9

--- Electron/dataMiners/trace.js
+++ Electron/dataMiners/trace.js
@@ -1,17 +1,24 @@
-const { exec } = require("child_process");
+const { spawn } = require("child_process");
 
-const command = `traceroute -m 30 -n google.com | awk 'BEGIN {print "["}; NR>1 {if ($2 ~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/) printf "{ \"hop_number\": %d, \"ip\": \"%s\" },\n", NR-1, $2}; END {print "]"}' | sed '$s/,$//'`;
+const traceroute = spawn("traceroute", ["-m", "30", "-n", "google.com"]);
+const awk = spawn("awk", [
+  'BEGIN {print "["}; NR>1 {if ($2 ~ /^[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$/) printf "{ \\"hop_number\\": %d, \\"ip\\": \\"%s\\" },\\n", NR-1, $2}; END {print "]"}',
+]);
+const sed = spawn("sed", ["$s/,$//"]);
 
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error executing command: ${error}`);
-    return;
+traceroute.stdout.pipe(awk.stdin);
+awk.stdout.pipe(sed.stdin);
+
+sed.stdout.on("data", (data) => {
+  console.log(`Output:\n${data}`);
+});
+
+sed.stderr.on("data", (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+sed.on("close", (code) => {
+  if (code !== 0) {
+    console.error(`Process exited with code ${code}`);
   }
-
-  if (stderr) {
-    console.error(`stderr: ${stderr}`);
-    return;
-  }
-
-  console.log(`Output:\n${stdout}`);
 });
