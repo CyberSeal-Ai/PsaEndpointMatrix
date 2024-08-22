bd11a6cbcf9bd01257333a540f1bc794c8e0e33e
Sudarsan <s_r@mt.iitr.ac.in>
2024-08-22 13:33:15 +0530

    mac#10

--- Electron/dataMiners/trace.js
+++ Electron/dataMiners/trace.js
@@ -1,4 +1,5 @@
 const { spawn } = require("child_process");
+const axios = require("axios");
 
 const traceroute = spawn("traceroute", ["-m", "30", "-n", "google.com"]);
 const awk = spawn("awk", [
@@ -9,8 +10,23 @@ const sed = spawn("sed", ["$s/,$//"]);
 traceroute.stdout.pipe(awk.stdin);
 awk.stdout.pipe(sed.stdin);
 
-sed.stdout.on("data", (data) => {
-  console.log(`Output:\n${data}`);
+sed.stdout.on("data", async (data) => {
+  const jsonData = JSON.parse(data);
+
+  // Iterate through each IP and fetch location details
+  for (const item of jsonData) {
+    try {
+      const locationData = await getPublicIpAndVpnInfo(item.ip);
+      item.location = locationData;
+      console.log(`IP: ${item.ip}, Location: ${JSON.stringify(locationData)}`);
+      
+    } catch (error) {
+      console.error(`Error fetching data for IP: ${item.ip}`, error);
+    }
+  }
+
+
+  console.log("Enriched JSON Output:\n", JSON.stringify(jsonData, null, 2));
 });
 
 sed.stderr.on("data", (data) => {
@@ -22,3 +38,16 @@ sed.on("close", (code) => {
     console.error(`Process exited with code ${code}`);
   }
 });
+
+async function getPublicIpAndVpnInfo(ipAddress) {
+  try {
+    const apiKey = "f7e4f0334814499e958bcf0ea939b2b5";
+    const vpnApiUrl = `https://vpnapi.io/api/${ipAddress}?key=${apiKey}`;
+    const vpnResponse = await axios.get(vpnApiUrl);
+    const vpnData = vpnResponse.data;
+    return vpnData;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
