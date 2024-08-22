face84857ea4b55182b1a95902607c138b16f35b
Sudarsan <s_r@mt.iitr.ac.in>
2024-08-22 15:51:55 +0530

    #34

--- Electron/dataMiners/trace.js
+++ Electron/dataMiners/trace.js
@@ -2,12 +2,13 @@ const { spawn } = require("child_process");
 
 const traceroute = spawn("traceroute", ["-m", "30", "-n", "google.com"]);
 const awk = spawn("awk", [
-  'BEGIN {print "["}; NR>1 {if ($2 ~ /^[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$/) printf "{ \\"hop_number\\": %d, \\"ip\\": \\"%s\\" },\\n", NR-1, $2}; END {print "]"}',
+  'NR>1 {if ($2 ~ /^[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$/) printf "Hop %d: %s\\n", NR-1, $2}',
 ]);
 
 let outputData = "";
 
 traceroute.stdout.pipe(awk.stdin);
+
 awk.stdout.on("data", (data) => {
   outputData += data.toString();
 });
@@ -22,29 +23,6 @@ awk.on("close", (code) => {
     return;
   }
 
-  try {
-    // Clean the output string
-    console.log("Raw Output:", outputData);
-    console.log(typeof outputData);
-    outputData = outputData.slice(0,-2); // Remove the trailing comma and newlin
-    outputData = outputData + "]"
-    console.log("Raw Output:", outputData);
-    outputData = JSON.stringify(outputData, null, 2); 
-    console.log("Cleaned Output:", outputData);
-    console.log(typeof outputData);
-    outputData = outputData.slice(0,-3) + "]"; // Remove the trailing comma and newline
-    console.log("Raw JSON Output:", outputData); // Log the raw output
-
-    // Parse the cleaned JSON string
-    const jsonData = JSON.parse(outputData);
-    console.log("Parsed JSON Output:", JSON.stringify(jsonData, null, 2)); // Log formatted JSON
-
-    // Access and log the 'ip' of each element
-    jsonData.forEach((element) => {
-      console.log(`IP: ${element.ip}`);
-    });
-  } catch (error) {
-    console.error("Failed to parse JSON data:", error);
-    console.log("Raw Output:\n", outputData);
-  }
+  // Output the string result
+  console.log("Traceroute Result:\n", outputData);
 });
