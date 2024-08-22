8caa2f91329c479325ebe4bc060f2e4ef3ffa27f
Sudarsan <s_r@mt.iitr.ac.in>
2024-08-22 12:58:14 +0530

    mac#8

--- Electron/dataMiners/trace.js
+++ Electron/dataMiners/trace.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
 
-const command = `traceroute -m 30 -n worldaz.tr.teams.microsoft.com | awk 'BEGIN {print "["}; NR>1 {if ($2 ~ /^[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$/) printf "{ \\"hop_number\\": %d, \\"ip\\": \\"%s\\" },\\n", NR-1, $2}; END {print "]"}' | sed '$s/,$//'`;
+const command = `traceroute -m 30 -n google.com | awk 'BEGIN {print "["}; NR>1 {if ($2 ~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/) printf "{ \"hop_number\": %d, \"ip\": \"%s\" },\n", NR-1, $2}; END {print "]"}' | sed '$s/,$//'`;
 
 exec(command, (error, stdout, stderr) => {
   if (error) {
