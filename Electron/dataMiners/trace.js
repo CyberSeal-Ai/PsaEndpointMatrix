c8c72baafb49d3e2c52683c71facaa6dfd1e93ce
Sudarsan <s_r@mt.iitr.ac.in>
2024-08-22 14:29:15 +0530

    mac#18

--- Electron/dataMiners/trace.js
+++ Electron/dataMiners/trace.js
@@ -1,10 +1,11 @@
+
 const { spawn } = require("child_process");
 
 const traceroute = spawn("traceroute", ["-m", "30", "-n", "google.com"]);
 const awk = spawn("awk", [
   'BEGIN {print "["}; NR>1 {if ($2 ~ /^[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$/) printf "{ \\"hop_number\\": %d, \\"ip\\": \\"%s\\" },\\n", NR-1, $2}; END {print "]"}',
 ]);
-const sed = spawn("sed", ["$s/,$//"]);
+const sed = spawn("sed", ["'$!{H;$!d} ; x ; s/},\\n]$/}\\n]/'"]);
 
 let outputData = "";
 
@@ -26,6 +27,8 @@ sed.on("close", (code) => {
   }
 
   try {
+    // Remove any trailing comma before the closing bracket
+    outputData = outputData.replace(/,\s*\]$/, "]");
     const jsonData = JSON.parse(outputData);
     console.log("Parsed JSON Output:", jsonData);
   } catch (error) {
